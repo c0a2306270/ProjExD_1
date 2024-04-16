4fcbcdc6e3a7f4c77813f03874a0693f4a64d6c7
komuro <c0a2306270@edu.teu.ac.jp>
2024-04-16 17:37:26 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -21,7 +21,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        key_lst = pg.key.get_pressed()
+        key_lst = pg.key.get_pressed()#練習８
         if key_lst[pg.K_UP]:
             kk_rct.move_ip([0,-1])
         if key_lst[pg.K_DOWN]:
@@ -29,7 +29,9 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip([-1,0])
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip([1,0])
+            kk_rct.move_ip([2,0])
+        else:
+            kk_rct.move_ip([-1,0])
         x=tmr%3200#練習６
         screen.blit(bg_img, [-x, 0])#練習６
         screen.blit(fbg_ing,[-x+1600, 0])#練習７
